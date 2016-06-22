54d3363472c4293fbcd442bd5dd84419e1632ba6
zxk901021 <447360581@qq.com>
2016-06-22 18:08:20 +0800

    20160622commit

--- 365EduPlatform/gen/com/example/view/R.java
+++ 365EduPlatform/gen/com/example/view/R.java
@@ -99,7 +99,18 @@ containing a value of this type.
          */
         public static final int ci_width=0x7f010000;
     }
+    public static final class color {
+        public static final int black_de=0x7f050004;
+        public static final int card_background=0x7f050000;
+        public static final int card_shadow=0x7f050001;
+        public static final int lite_blue=0x7f050002;
+        public static final int window_background=0x7f050003;
+    }
     public static final class dimen {
+        public static final int action_button_height=0x7f060002;
+        public static final int action_button_min_width=0x7f060004;
+        public static final int action_button_padding_horizontal=0x7f060005;
+        public static final int action_button_text_size=0x7f060003;
         /**  Default screen margins, per the Android Design guidelines. 
 
          Example customization of dimensions originally defined in res/values/dimens.xml
@@ -107,55 +118,71 @@ containing a value of this type.
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int edu_launcher_1=0x7f020000;
-        public static final int edu_launcher_2=0x7f020001;
-        public static final int edu_launcher_3=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int launch_btn=0x7f020004;
-        public static final int white_radius=0x7f020005;
+        public static final int bg_button_normal=0x7f020000;
+        public static final int bg_button_pressed=0x7f020001;
+        public static final int button=0x7f020002;
+        public static final int edu_launcher_1=0x7f020003;
+        public static final int edu_launcher_2=0x7f020004;
+        public static final int edu_launcher_3=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int launch_btn=0x7f020007;
+        public static final int meterial_card=0x7f020008;
+        public static final int meterial_dialog_window=0x7f020009;
+        public static final int white_radius=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f090014;
-        public static final int bottom=0x7f090002;
-        public static final int center=0x7f090003;
-        public static final int center_horizontal=0x7f090004;
-        public static final int center_vertical=0x7f090005;
-        public static final int clip_horizontal=0x7f090006;
-        public static final int clip_vertical=0x7f090007;
-        public static final int end=0x7f090008;
-        public static final int fill=0x7f090009;
-        public static final int fill_horizontal=0x7f09000a;
-        public static final int fill_vertical=0x7f09000b;
-        public static final int horizontal=0x7f090000;
-        public static final int launch_indicator=0x7f090012;
-        public static final int launch_pager=0x7f090011;
-        public static final int launch_start=0x7f090013;
-        public static final int left=0x7f09000c;
-        public static final int main_web=0x7f090010;
-        public static final int right=0x7f09000d;
-        public static final int start=0x7f09000e;
-        public static final int top=0x7f09000f;
-        public static final int vertical=0x7f090001;
+        public static final int action_settings=0x7f0a001d;
+        public static final int bottom=0x7f0a0002;
+        public static final int btn_n=0x7f0a001b;
+        public static final int btn_p=0x7f0a001c;
+        public static final int buttonLayout=0x7f0a001a;
+        public static final int center=0x7f0a0003;
+        public static final int center_horizontal=0x7f0a0004;
+        public static final int center_vertical=0x7f0a0005;
+        public static final int clip_horizontal=0x7f0a0006;
+        public static final int clip_vertical=0x7f0a0007;
+        public static final int contentView=0x7f0a0015;
+        public static final int end=0x7f0a0008;
+        public static final int fill=0x7f0a0009;
+        public static final int fill_horizontal=0x7f0a000a;
+        public static final int fill_vertical=0x7f0a000b;
+        public static final int horizontal=0x7f0a0000;
+        public static final int launch_indicator=0x7f0a0012;
+        public static final int launch_pager=0x7f0a0011;
+        public static final int launch_start=0x7f0a0013;
+        public static final int left=0x7f0a000c;
+        public static final int main_web=0x7f0a0010;
+        public static final int material_background=0x7f0a0014;
+        public static final int message=0x7f0a0019;
+        public static final int message_content_root=0x7f0a0017;
+        public static final int message_content_view=0x7f0a0018;
+        public static final int right=0x7f0a000d;
+        public static final int start=0x7f0a000e;
+        public static final int title=0x7f0a0016;
+        public static final int top=0x7f0a000f;
+        public static final int vertical=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash=0x7f030001;
+        public static final int layout_meterial_dialog=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int splash=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int splash=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
-        public static final int title_activity_main=0x7f060003;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070001;
+        public static final int title_activity_main=0x7f070003;
     }
     public static final class style {
+        public static final int ActionButton=0x7f080002;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -177,11 +204,11 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class styleable {
         /** Attributes that can be used with a CircleIndicator.
--- 365EduPlatform/src/com/example/view/MainActivity.java
+++ 365EduPlatform/src/com/example/view/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.view;
 
+import java.util.Set;
+
+import com.zhy_9.edu_platform.util.VersionDialogUtil;
+import com.zhy_9.edu_platform.view.MaterialDialog;
+
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.CookieManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -22,14 +33,40 @@ public class MainActivity extends Activity {
 	public static final String JAVA_SCRIPT_INTERFACE_METHOD_NAME = "wst";
 	public static final String EDU_PLATFORM_URL = "http://192.168.1.188/wb/api/install";
 	private String registerId;
-
+	CookieManager cookieManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
 		getRegisterId();
+		setAliasAndTags();
 		webConfigSetting();
+		cookieManager = CookieManager.getInstance();
+		String cookies = cookieManager.getCookie(EDU_PLATFORM_URL);
+		if (!TextUtils.isEmpty(cookies)) {
+			Log.e("cookies", cookies);
+		}
+//		VersionDialogUtil.isNeedUpdate(this);
+		MaterialDialog dialog = new MaterialDialog(this);
+		dialog.setTitle("更新");
+		dialog.setMessage("有新更新");
+		dialog.setPositiveButton("确定", new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		dialog.setNegativeButton("取消", new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+			}
+		});
+		dialog.show();
 	}
 
 	private void initView() {
@@ -39,8 +76,22 @@ public class MainActivity extends Activity {
 	private void getRegisterId() {
 		registerId = JPushInterface.getRegistrationID(this);
 	}
+	
+	private void setAliasAndTags(){
+		if (!TextUtils.isEmpty(registerId)) {
+			JPushInterface.setAlias(this, registerId, new TagAliasCallback() {
+				
+				@Override
+				public void gotResult(int arg0, String arg1, Set<String> arg2) {
+					
+				}
+			});
+		}else {
+			
+		}
+	}
 
-	private void webConfigSetting() {
+	@SuppressLint("SetJavaScriptEnabled") private void webConfigSetting() {
 		mainWeb.setWebViewClient(mWebViewClient);
 		webSettings = mainWeb.getSettings();
 		webSettings.setJavaScriptEnabled(true);
@@ -52,6 +103,16 @@ public class MainActivity extends Activity {
 	@JavascriptInterface
 	public String startFunction(String str) {
 		return registerId;
+	}
+	
+	@JavascriptInterface
+	public void setFunction(String str){
+		if (!TextUtils.isEmpty(str)) {
+			Log.e("setFunction", str);
+		}else {
+			Log.e("setFunction", "str = null");
+		}
+		
 	}
 
 	private WebViewClient mWebViewClient = new WebViewClient() {
